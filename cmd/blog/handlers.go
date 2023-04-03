de08bc10d13c44704d7fcf77336bdb0f204dd52d
Korta Esaroz <kortaesaroz@gmail.com>
2023-04-03 16:12:03 +0300

    sql_added

--- cmd/blog/handlers.go
+++ cmd/blog/handlers.go
@@ -4,147 +4,118 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"github.com/jmoiron/sqlx"
 )
 
-type indexPage struct {
-	FeaturedPosts []featuredPostData
-	MostRecent    []mostRecentData
+type indexPageData struct {
+	Title         string
+	Subtitle      string
+	FeaturedPosts []FeaturedPosts
+	RecentPosts []RecentPosts
 }
 
-type postPage struct {
+type FeaturedPosts struct {
+	Title       string `db:"title"`
+	Subtitle    string `db:"subtitle"`
+	PublishDate string `db:"publish_date"`
+	Author      string `db:"author"`
+	AuthorUrl   string `db:"author_url"`
+	ImageUrl    string `db:"image_url"`
+	Featured    string `db:"featured"`
 }
 
-type featuredPostData struct {
-	Title                   string
-	Subtitle                string
-	BackgroundImageModifier string
-	AuthorAvatar            string
-	AuthorName              string
-	PublishDate             string
-	PostButton              string
-	ButtonClass             string
+type RecentPosts struct {
+	Title       string `db:"title"`
+	Subtitle    string `db:"subtitle"`
+	PublishDate string `db:"publish_date"`
+	Author      string `db:"author"`
+	AuthorUrl   string `db:"author_url"`
+	ImageUrl    string `db:"image_url"`
+	Featured    string `db:"featured"`
 }
 
-type mostRecentData struct {
-	PhotoImage   string
-	Title        string
-	Subtitle     string
-	AuthorName   string
-	AuthorAvatar string
-	PublishDate  string
-}
 
-func index(w http.ResponseWriter, r *http.Request) {
-	ts, err := template.ParseFiles("pages/index.html") // Главная страница блога
-	if err != nil {
-		http.Error(w, "Internal Server Error", 500) // В случае ошибки парсинга - возвращаем 500
-		log.Println(err.Error())                    // Используем стандартный логгер для вывода ошбики в консоль
-		return                                      // Не забываем завершить выполнение ф-ии
-	}
+func index(db *sqlx.DB) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		posts, err := featuredPosts(db)
+		rposts, err := mostRecent(db)
+		if err != nil {
+			http.Error(w, "Internal Server Error", 500) // В случае ошибки парсинга - возвращаем 500
+			log.Println(err)
+			return // Не забываем завершить выполнение ф-ии
+		}
 
-	data := indexPage{
-		FeaturedPosts: featuredPosts(),
-		MostRecent:    mostRecent(),
-	}
+		ts, err := template.ParseFiles("pages/index.html") // Главная страница блога
+		if err != nil {
+			http.Error(w, "Internal Server Error", 500) // В случае ошибки парсинга - возвращаем 500
+			log.Println(err)
+			return // Не забываем завершить выполнение ф-ии
+		}
 
-	err = ts.Execute(w, data) // Заставляем шаблонизатор вывести шаблон в тело ответа
-	if err != nil {
-		http.Error(w, "Internal Server Error", 500)
-		log.Println(err.Error())
-		return
-	}
-}
+		data := indexPageData{
+			Title:         "Blog for traveling",
+			Subtitle:      "My best blog for adventures and burgers",
+			FeaturedPosts: posts,
+			RecentPosts: rposts,
+		}
 
-func post(w http.ResponseWriter, r *http.Request) {
-	ts, err := template.ParseFiles("pages/post.html")
-	if err != nil {
-		http.Error(w, "Internal Server Error", 500)
-		log.Println(err.Error())
-		return
-	}
+		err = ts.Execute(w, data) // Заставляем шаблонизатор вывести шаблон в тело ответа
+		if err != nil {
+			http.Error(w, "Internal Server Error", 500)
+			log.Println(err)
+			return
+		}
 
-	data := postPage{}
-
-	err = ts.Execute(w, data)
-	if err != nil {
-		http.Error(w, "Internal Server Error", 500)
-		log.Println(err.Error())
-		return
+		log.Println("Request completed successfully")
 	}
 }
 
-func featuredPosts() []featuredPostData {
-	return []featuredPostData{
-		{
-			Title:                   "The Road Ahead",
-			Subtitle:                "The road ahead might be paved - it might not be.",
-			BackgroundImageModifier: "featured__post_background-road",
-			AuthorAvatar:            "/static/images/mat_vogels.jpg",
-			AuthorName:              "Mat Vogels",
-			PublishDate:             "September 25, 2015",
-		},
-		{
-			Title:                   "From Top Down",
-			Subtitle:                "Once a year, go someplace you’ve never been before.",
-			BackgroundImageModifier: "featured__post_background-fromtop",
-			AuthorAvatar:            "/static/images/william_wong.jpg",
-			AuthorName:              "William Wong",
-			PublishDate:             "September 25, 2015",
-			PostButton:              "ADVENTURE",
-			ButtonClass:             "post__button",
-		},
+func featuredPosts(db *sqlx.DB) ([]FeaturedPosts, error) {
+	const query = `
+		SELECT
+			title,
+			subtitle,
+			author,
+			author_url,
+			publish_date,
+			image_url,
+			featured
+		FROM
+			post
+		WHERE featured = 1
+	` // Составляем SQL-запрос для получения записей для секции featured-posts
+
+	var posts []FeaturedPosts // Заранее объявляем массив с результирующей информацией
+
+	err := db.Select(&posts, query) // Делаем запрос в базу данных
+	if err != nil {                 // Проверяем, что запрос в базу данных не завершился с ошибкой
+		return nil, err
 	}
+
+	return posts, nil
 }
 
-func mostRecent() []mostRecentData {
-	return []mostRecentData{
-		{
-			PhotoImage:   "/static/images/still_standing_tall.jpg",
-			Title:        "Still Standing Tall",
-			Subtitle:     "Life begins at the end of your comfort zone.",
-			AuthorName:   "William Wong",
-			AuthorAvatar: "/static/images/william_wong.jpg",
-			PublishDate:  "9/25/2015",
-		},
-		{
-			PhotoImage:   "/static/images/sunny_side_up.jpg",
-			Title:        "Sunny Side Up",
-			Subtitle:     "No place is ever as bad as they tell you it’s going to be.",
-			AuthorName:   "Mat Vogels",
-			AuthorAvatar: "/static/images/mat_vogels.jpg",
-			PublishDate:  "9/25/2015",
-		},
-		{
-			PhotoImage:   "/static/images/water_falls.jpg",
-			Title:        "Water Falls",
-			Subtitle:     "We travel not to escape life, but for life not to escape us.",
-			AuthorName:   "William Wong",
-			AuthorAvatar: "/static/images/william_wong.jpg",
-			PublishDate:  "9/25/2015",
-		},
-		{
-			PhotoImage:   "/static/images/through_the_mist.jpg",
-			Title:        "Through the Mist",
-			Subtitle:     "Travel makes you see what a tiny place you occupy in the world.",
-			AuthorName:   "William Wong",
-			AuthorAvatar: "/static/images/william_wong.jpg",
-			PublishDate:  "9/25/2015",
-		},
-		{
-			PhotoImage:   "/static/images/still_standing_tall.jpg",
-			Title:        "Still Standing Tall",
-			Subtitle:     "Life begins at the end of your comfort zone.",
-			AuthorName:   "William Wong",
-			AuthorAvatar: "../../static/images/william_wong.jpg",
-			PublishDate:  "9/25/2015",
-		},
-		{
-			PhotoImage:   "/static/images/awaken_early.jpg",
-			Title:        "Awaken Early",
-			Subtitle:     "Not all those who wander are lost.",
-			AuthorName:   "Mat Vogels",
-			AuthorAvatar: "/static/images/mat_vogels.jpg",
-			PublishDate:  "9/25/2015",
-		},
+func mostRecent(db *sqlx.DB) ([]RecentPosts, error) {
+	const query = `
+		SELECT
+			title,
+			subtitle,
+			author,
+			author_url,
+			publish_date,
+			image_url,
+			featured
+		FROM
+			post
+		WHERE featured = 0
+	` // Составляем SQL-запрос для получения записей для секции featured-posts
+
+	var rposts []RecentPosts // Заранее объявляем массив с результирующей информацией
+
+	err := db.Select(&rposts, query) // Делаем запрос в базу данных
+	if err != nil {                 // Проверяем, что запрос в базу данных не завершился с ошибкой
+		return nil, err
 	}
+
+	return rposts, nil
 }
--- cmd/blog/main.go
+++ cmd/blog/main.go
@@ -1,25 +1,41 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
+
+	_ "github.com/go-sql-driver/mysql" // Импортируем для возможности подключения к MySQL
+	"github.com/jmoiron/sqlx"
 )
 
 const (
-	port = ":3000"
+	port         = ":3000"
+	dbDriverName = "mysql"
 )
 
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/home", index)
-	mux.HandleFunc("/post", post)
-
-	// Реализуем отдачу статики
-	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
-
-	log.Println("Start server " + port)
-	err := http.ListenAndServe(port, mux)
+	db, err := openDB() // Открываем соединение к базе данных в самом начале
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	dbx := sqlx.NewDb(db, dbDriverName) // Расширяем стандартный клиент к базе
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/home", index(dbx)) // Передаём клиент к базе данных в ф-ию обработчик запроса
+
+	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+
+	fmt.Println("Start server")
+	err = http.ListenAndServe(port, mux)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func openDB() (*sql.DB, error) {
+	// Здесь прописываем соединение к базе данных
+	return sql.Open(dbDriverName, "root:root123321@tcp(localhost:3306)/blog?charset=utf8mb4&collation=utf8mb4_unicode_ci&parseTime=true")
 }
